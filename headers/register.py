54658eb35d3c9dbf088d68827313917718f0320b
Kalen Willits <KalenWillits@gmail.com>
2022-07-06 19:55:48 -0400

    Adding online / offline messages

--- headers/register.py
+++ headers/register.py
@@ -1,11 +1,15 @@
+import asyncio
+import orjson
+
 from lexicons import Header
 from lexicons.utils import encrypt
 
 
 class Register(Header):
-    def execute(self, db=None, ws=None, username=None, password=None, **kwargs):
+    def execute(self, db=None, ws=None, sv=None, username=None, password=None, **kwargs):
         if db.query('User', username=username).empty:
             user = db.create('User', username=username, password=encrypt(password))
             ws.auth = user.pk
+            asyncio.ensure_future(ws.send(orjson.dumps([{'Register': {'success': True}}])))
 
         return False
--- main.py
+++ main.py
@@ -9,7 +9,19 @@ from signals.create_chat import CreateChat
 from signals.get_chat_list import GetChatList
 
 
-sv = Server(Public, Login, Register, Message, User, CreateChat, GetChatList)
+def on_disconnect(db=None, sv=None, ws=None):
+    user = db.get('User', ws.auth)
+    payload = {'UserExit': f'{user.username} is now offline...'}
+    sv.broadcast(payload, ['Public'])
+
+def on_connect(db=None, sv=None, ws=None):
+    user = db.get('User', ws.auth)
+    payload = {'UserEnter': f'{user.username} has come online...'}
+    sv.broadcast(payload, ['Public'])
+
+
+sv = Server(Public, Login, Register, Message, User, CreateChat, GetChatList, on_connect=on_connect,
+            on_disconnect=on_disconnect)
 
 
 if __name__ == '__main__':
